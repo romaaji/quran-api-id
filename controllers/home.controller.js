ed224549c1491d223126eccb0bf2750e7c6a1fba
Romadhon Aji <60551839+romaaji@users.noreply.github.com>
2022-04-21 23:06:38 +0700

    Update home.controller.js

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -2,26 +2,26 @@ const config = require("../config/config");
 
 const endpoints = [
   {
-    path: `${config.BASE_URL}/surahs`,
+    path: `http://quran.romaa.my.id/surahs`,
     description: "dapatkan semua surah: /surahs",
   },
   {
-    path: `${config.BASE_URL}/surahs/112`,
+    path: `http://quran.romaa.my.id/surahs/112`,
     description:
       "dapatkan surah tertentu menggunakan nomor surah dalam quran (1 - 114): /surahs/{nomorSurah}",
   },
   {
-    path: `${config.BASE_URL}/surahs/112/ayahs`,
+    path: `http://quran.romaa.my.id/surahs/112/ayahs`,
     description:
       "dapatkan semua ayat dari surah tertentu: /surahs/{numberSurah}/ayahs",
   },
   {
-    path: `${config.BASE_URL}/surahs/112/ayahs/2`,
+    path: `http://quran.romaa.my.id/surahs/112/ayahs/2`,
     description:
       "dapatkan ayat tertentu dari surah tertentu: /surahs/{numberSurah}/ayahs/{numberAyah}",
   },
   {
-    path: `${config.BASE_URL}/random`,
+    path: `http://quran.romaa.my.id/random`,
     description: "dapatkan ayat acak: /random",
   },
 ];
